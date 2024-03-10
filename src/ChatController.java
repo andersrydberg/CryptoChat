72bb4816eaec5125d4c841aec67d2552e5feb2e9
Anders Rydberg <andersrydberg@yahoo.com>
2024-03-10 12:32:10 +0100

    Added fields, handler methods to controller

--- src/ChatController.java
+++ src/ChatController.java
@@ -1,7 +1,27 @@
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
 public class ChatController {
+
+    public TextField ipField;
+    public Button mainButton;
+    public Text displayText;
+    public VBox publicKeyBox;
+    public TextField ownKeyField;
+    public TextField contactPublicKey;
+    public TextArea chatArea;
+    public TextField chatInputField;
+
+    public void buttonHandler(ActionEvent event) {
+
+    }
+
+    public void sendMessageHandler(ActionEvent event) {
+
+    }
 
 }
