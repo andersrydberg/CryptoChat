3194d46698216addbd8a77c2e1f9535ba78b6262
Anders Rydberg <andersrydberg@yahoo.com>
2024-05-04 20:20:12 +0200

    More functionality on Backend

--- src/Backend.java
+++ src/Backend.java
@@ -1,4 +1,10 @@
+import javafx.application.Platform;
+import javafx.concurrent.Task;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class Backend implements Runnable {
@@ -27,18 +33,53 @@ public class Backend implements Runnable {
 
     /**
      * check if there is an ongoing session, else prompt user
+     *
      * @param socket
      */
     public void tryConnection(Socket socket) {
-        if (ongoingSession == null) {
-            try {
-                socket.close();
-            } catch (IOException e) {
-                // ignore
-            }
+
+        if (ongoingSession != null) {
+            closeConnection(socket);
+            // log: request from ... rejected: ongoing session
+            return;
         }
 
         // prompt user...
+        final String inetAddress = socket.getInetAddress().toString();
+        var confirm = new Task<Boolean>() {
+            @Override
+            protected Boolean call() throws Exception {
+                var result = new Alert(Alert.AlertType.CONFIRMATION,
+                        String.format("Accept connection from %s?", inetAddress))
+                        .showAndWait();
+                return (result.isPresent() && result.get() == ButtonType.OK);
+            }
+        };
+
+        Platform.runLater(confirm);
+
+        try {
+            if (confirm.get()) {
+                ongoingSession = socket;
+                // log: connection with ... established
+            } else {
+                closeConnection(socket);
+                // log: request from ... denied
+            }
+        } catch (Exception e) {
+            closeConnection(socket);
+            // log: could not establish connection to ...
+        }
+    }
+
+    private void closeConnection(Socket socket) {
+        try (socket) {
+            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+            oos.writeObject(Message.DECLINED);
+            oos.flush();
+        } catch (IOException e) {
+            // ignore
+        }
     }
 
     // methods for reacting to user input
