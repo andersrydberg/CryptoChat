b3d60cb6b297964c543b442fdb39652faa687160
Anders Rydberg <andersrydberg@yahoo.com>
2024-05-04 08:53:54 +0200

    New version of Server

--- src/Backend.java
+++ src/Backend.java
@@ -1,5 +1,10 @@
+import java.io.IOException;
+import java.net.Socket;
+
 public class Backend implements Runnable {
     private final ChatController chatController;
+    private Server2 server;
+    private Socket ongoingSession;
 
     public Backend(ChatController chatController) {
         this.chatController = chatController;
@@ -8,10 +13,33 @@ public class Backend implements Runnable {
     @Override
     public void run() {
         // start server
+        server = new Server2(this);
 
+        Thread thread = new Thread(server);
+        thread.setDaemon(true);
+        thread.start();
     }
 
+
     // methods for reacting to server events
+    public void serverStartupError() {
+    }
+
+    /**
+     * check if there is an ongoing session, else prompt user
+     * @param socket
+     */
+    public void tryConnection(Socket socket) {
+        if (ongoingSession == null) {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                // ignore
+            }
+        }
+
+        // prompt user...
+    }
 
     // methods for reacting to user input
 }
---
+++ src/Server2.java
@@ -0,0 +1,50 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+public class Server2 implements Runnable {
+
+
+    private static final int DEFAULT_PORT = 27119;
+    private final Backend backend;
+    private ServerSocket serverSocket;
+    private boolean active = true;
+
+    public Server2(Backend backend) {
+        this.backend = backend;
+    }
+
+    @Override
+    public void run() {
+
+        ServerSocket serverSocket;
+        try {
+            serverSocket = new ServerSocket(DEFAULT_PORT);
+            serverSocket.setSoTimeout(2000);
+        } catch (IOException e) {
+            e.printStackTrace();
+            backend.serverStartupError();
+            return;
+        }
+
+
+        while (active) {
+            try {
+                Socket socket = serverSocket.accept();
+                backend.tryConnection(socket);
+
+            } catch (SocketTimeoutException e) {
+                // ignore
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+
+    }
+
+    public void deactivate() {
+        active = false;
+    }
+}
